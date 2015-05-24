85253dc57320425d3e86f75131d8342b576c1aa9
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-24 09:42:01 +0200

    Use `meow` in the CLI

--- cli.js
+++ cli.js
@@ -1,29 +1,15 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var currentPath = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    $ current-path',
-		'    /Users/sindresorhus/dev/current-path'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  $ current-path',
+		'  /Users/sindresorhus/dev/current-path'
+	].join('\n')
+});
 
 currentPath(function (err, path) {
 	if (err) {
